42bd14e0a33c6259239329996cd1bf51639f9a39
Lydia Ho <lyd.w.ho@gmail.com>
2017-03-13 12:08:54 -0700

    RecurringEvent adjusts delivery dates for weekends and bank holidays

--- app/models/recurring_event.rb
+++ app/models/recurring_event.rb
@@ -1,24 +1,37 @@
 class RecurringEvent < ApplicationRecord
-  CALCULATED_DAY_OF_MONTH = 19
+  BANK_HOLIDAYS = [
+      Date.new(2017, 1, 2),   Date.new(2017, 1, 16),  Date.new(2017, 2, 20),  Date.new(2017, 5, 29),
+      Date.new(2017, 7, 4),   Date.new(2017, 9, 4),   Date.new(2017, 10, 9),  Date.new(2017, 11, 11),
+      Date.new(2017, 11, 23), Date.new(2017, 12, 25), Date.new(2018, 1, 1),   Date.new(2018, 1, 15),
+      Date.new(2018, 2, 19),  Date.new(2018, 5, 28),  Date.new(2018, 7, 4),   Date.new(2018, 9, 3),
+      Date.new(2018, 10, 8),  Date.new(2018, 11, 12), Date.new(2018, 11, 22), Date.new(2018, 12, 25)
+  ]
 
   def calculated_date
-    CALCULATED_DAY_OF_MONTH
+    start_date
   end
 
   def delivery_date
-    month = start_date.month
-    day = calculated_date
-    year = start_date.year
-    adjust_if_weekend(Date.new(year, month, day))
+    delivery = adjust_if_weekend(calculated_date)
+    check_if_bank_holiday(delivery)
   end
 
   private
 
   def adjust_if_weekend(delivery_date)
     if delivery_date.saturday?
-      delivery_date + 2.days
+      delivery_date - 1.day
     elsif delivery_date.sunday?
-      delivery_date + 1.day
+      delivery_date - 2.days
+    else
+      delivery_date
+    end
+  end
+
+  def check_if_bank_holiday(delivery_date)
+    if BANK_HOLIDAYS.include?(delivery_date)
+      delivery = delivery_date - 1.day
+      adjust_if_weekend(delivery)
     else
       delivery_date
     end
--- spec/models/recurring_event_spec.rb
+++ spec/models/recurring_event_spec.rb
@@ -9,11 +9,16 @@ RSpec.describe RecurringEvent, type: :model do
   it "has a calculated date based on the start date" do
     recurring_event = RecurringEvent.create(name: "plant watering", start_date: Date.new(2017, 3, 11), number_months: 1)
     calculated_date = recurring_event.calculated_date
-    expect(calculated_date).to eq(19)
+    expect(calculated_date).to eq(Date.new(2017, 3, 11))
   end
 
   it "has a delivery date based on the calculated date" do
-    recurring_event = RecurringEvent.create(name: "plant watering", start_date: Date.new(2017, 3, 11), number_months: 1)
-    expect(recurring_event.delivery_date).to eq(Date.new(2017, 3, 20))
+    recurring_event = RecurringEvent.create(name: "plant watering", start_date: Date.new(2017, 3, 19), number_months: 1)
+    expect(recurring_event.delivery_date).to eq(Date.new(2017, 3, 17))
+  end
+
+  it "adjusts delivery date if the delivery date is on a bank holidays" do
+    recurring_event = RecurringEvent.create(name: "plant watering", start_date: Date.new(2017, 1, 2), number_months: 1)
+    expect(recurring_event.delivery_date).to eq(Date.new(2016, 12, 30))
   end
 end
