caea77a8fd305522d931e8addd4ab6ee90aa626d
Lydia Ho <lyd.w.ho@gmail.com>
2017-03-13 18:48:26 -0700

    Remove comment

--- app/jobs/next_event_creation_job.rb
+++ app/jobs/next_event_creation_job.rb
@@ -2,7 +2,6 @@ class NextEventCreationJob < ApplicationJob
   queue_as :default
 
   def perform(event)
-    puts "!!!!!!!!!!!!!!!!!!! MAKING A NEW EVENT !!!!!!!!!!!!!!!!"
     event = Event.new(delivery_date: event.next_delivery_date, recurring_event_id: event.recurring_event_id)
     event.save!
   end
